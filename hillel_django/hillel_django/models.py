d3689ba1c23c1cc5889a28fc91d3b92bde764251
Holodi <88102380+Holodi@users.noreply.github.com>
2024-03-04 15:24:02 +0000

    Add files via upload

--- hillel_django/hillel_django/models.py
+++ hillel_django/hillel_django/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django import forms
 
 
-class User(models.Model):
+
+class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+    groups = models.ManyToManyField(Group, related_name='custom_users', blank=True)  # Додано related_name
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_users', blank=True)  # Додано related_name
+
+    class Meta:
+        permissions = [
+            ("custom_groups", "Can view custom groups"),
+            ("custom_user_permissions", "Can view custom user permissions"),
+        ]
 
 
 class Lesson(models.Model):
@@ -19,4 +31,22 @@ class Word(models.Model):
     translation = models.CharField(max_length=100)
     transliteration = models.CharField(max_length=100)
     phonetic_transcription = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='words')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_words')
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['last_name', 'first_name', 'email', 'password1', 'password2']
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password']
+
+
+class WordForm(forms.ModelForm):
+    class Meta:
+        model = Word
+        fields = ['word', 'translation', 'transliteration', 'phonetic_transcription']
--- hillel_django/views.py
+++ hillel_django/views.py
@@ -1,64 +1,59 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.db.models import Sum
 
-from hillel_django import models
-from hillel_django.models import Lesson, Word
-from django.contrib import messages
+from hillel_django.models import Lesson, Word, UserRegisterForm, UserLoginForm, WordForm, CustomUser  # Змінено імпорт
+
 
 def register_user(request):
     if request.method == 'POST':
-        last_name = request.POST['last_name']
-        first_name = request.POST['first_name']
-        email = request.POST['email']
-        password = request.POST['password']
-        # Перевірка на унікальність електронної пошти
-        if User.objects.filter(email=email).exists():
-            return HttpResponse('Ця електронна пошта вже використовується!')
-        # Створення користувача
-        User.objects.create_user(last_name=last_name, first_name=first_name, email=email, password=password)
-        return HttpResponse('Користувач зареєстрований успішно!')
-    return render(request, 'register.html')
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Користувач зареєстрований успішно!')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
 
 def login_user(request):
     if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        # Перевірка введених даних
-        user = authenticate(username=email, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponse('Вхід успішний!')
-        else:
-            return HttpResponse('Неправильна електронна пошта або пароль!')
-    return render(request, 'login.html')
+        form = UserLoginForm(request, data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponse('Вхід успішний!')
+            else:
+                return HttpResponse('Неправильна електронна пошта або пароль!')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
 
 @login_required
 def user_profile(request):
     user = request.user
     if request.method == 'POST':
-        # Отримуємо дані форми з POST-запиту
-        last_name = request.POST.get('last_name')
-        first_name = request.POST.get('first_name')
-        # Отримуємо поточного користувача
-        user.last_name = last_name
-        user.first_name = first_name
-        user.save()
-        return redirect('user_profile')  # Перенаправляємо користувача на сторінку профілю
-    return render(request, 'user_profile.html', {'user': user})
+        form = UserRegisterForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Профіль оновлено успішно!')
+            return redirect('user_profile')
+    else:
+        form = UserRegisterForm(instance=user)
+    return render(request, 'user_profile.html', {'form': form})
 
 @login_required
 def delete_user(request):
     if request.method == 'POST':
-        # Отримуємо поточного користувача
         user = request.user
-        # Видаляємо користувача
         user.delete()
-        return redirect('login')  # Перенаправляємо користувача на сторінку входу
+        return redirect('login')
     else:
-        # Повертаємо сторінку з підтвердженням видалення
         return render(request, 'delete_confirmation.html')
 
 @login_required
@@ -68,12 +63,9 @@ def lesson_list(request):
 
 @login_required
 def lesson_detail(request, id):
-    lesson = Lesson.objects.get(id=id)
+    lesson = get_object_or_404(Lesson, id=id)
     if request.method == 'POST':
-        # Отримуємо дані результатів уроку з POST-запиту
         results = request.POST.getlist('results[]')
-        # Логіка для збереження результатів уроку
-        # Наприклад, ми можемо оновити атрибути уроку з результатами
         lesson.results = results
         lesson.save()
         return JsonResponse({'message': 'Результати уроку збережено успішно!'})
@@ -82,36 +74,34 @@ def lesson_detail(request, id):
 
 @login_required
 def top_score(request):
-    # Отримуємо список користувачів та сортуємо їх за сумою балів
-    users = User.objects.annotate(total_score=models.Sum('lesson__result')).order_by('-total_score')
+    users = CustomUser.objects.annotate(total_score=Sum('lesson__result')).order_by('-total_score')
     return render(request, 'top_score.html', {'users': users})
+
 
 @login_required
 def word_list(request):
-    # Отримуємо список слів поточного користувача
     user = request.user
-    words = user.words.all()
+    words = user.user_words.all()
     if request.method == 'POST':
-        # Обробляємо додавання нового слова
-        word = request.POST.get('word')
-        translation = request.POST.get('translation')
-        transliteration = request.POST.get('transliteration')
-        phonetic_transcription = request.POST.get('phonetic_transcription')
-        new_word = Word.objects.create(word=word, translation=translation, transliteration=transliteration, phonetic_transcription=phonetic_transcription, user=user)
-        return redirect('word_list')
-    return render(request, 'word_list.html', {'words': words})
+        form = WordForm(request.POST)
+        if form.is_valid():
+            word = form.save(commit=False)
+            word.user = user
+            word.save()
+            return redirect('word_list')
+    else:
+        form = WordForm()
+    return render(request, 'word_list.html', {'words': words, 'form': form})
+
 
 @login_required
 def word_detail(request, id):
-    # Отримуємо деталі конкретного слова
     word = get_object_or_404(Word, id=id)
     if request.method == 'POST':
-        # Обробляємо перевірку відповіді користувача
         answer = request.POST.get('answer')
         if answer.lower() == word.translation.lower():
-            # Логіка для правильної відповіді
             messages.success(request, 'Правильна відповідь!')
         else:
-            # Логіка для неправильної відповіді
             messages.error(request, 'Неправильна відповідь. Спробуйте ще раз.')
     return render(request, 'word_detail.html', {'word': word})
+
